df6aefbc9dd8714829953e34d89a74adb17381c0
= <=>
2019-11-07 23:24:54 +0700

    update

--- EnglishTest/config/quizz.routes.js
+++ EnglishTest/config/quizz.routes.js
@@ -12,12 +12,25 @@ quizzRoutes.route('/').get((req, res) => {
     })
 });
 quizzRoutes.route('/create').post((req, res) => {
-  console.log(req.body);
-  Quizz.create(req.body, (error, data) => {
+  let quizz = {}
+  quizz.code = req.body.code;
+  quizz.content = req.body.content;
+  quizz.code = req.body.code;
+  quizz.level = req.body.level;
+  quizz.listAnser =  [ 
+    {
+        "A" : req.body.answersA,
+        "B" : req.body.answersB,
+        "C" : req.body.answersC,
+        "D" : req.body.answersD
+    }
+  ]
+  console.log(quizz);
+  Quizz.create(quizz, (error, data) => {
   
     if (error) {
       console.log("error");
-      return next(error)
+      return error
     } else {
       res.json(data)
     }
@@ -25,6 +38,8 @@ quizzRoutes.route('/create').post((req, res) => {
 });
 quizzRoutes.route('/read/:id').get((req, res) => {
   Quizz.findById(req.params.id, (error, data) => {
+    console.log(data);
+    console.log(data.listAnser[0].A)
     if (error) {
      
       return next(error)
@@ -34,8 +49,22 @@ quizzRoutes.route('/read/:id').get((req, res) => {
   })
 });
 quizzRoutes.route('/update/:id').put((req, res, next) => {
+  let quizz = {}
+  quizz.code = req.body.code;
+  quizz.content = req.body.content;
+  quizz.code = req.body.code;
+  quizz.level = req.body.level;
+  quizz.listAnser =  [ 
+    {
+        "A" : req.body.answersA,
+        "B" : req.body.answersB,
+        "C" : req.body.answersC,
+        "D" : req.body.answersD
+    }
+  ]
+  console.log(quizz);
   Quizz.findByIdAndUpdate(req.params.id, {
-    $set: req.body
+    $set: quizz
   }, (error, data) => {
     if (error) {
       return next(error);
@@ -46,15 +75,14 @@ quizzRoutes.route('/update/:id').put((req, res, next) => {
     }
   })
 });
-quizzRoutes.route('/delete/:id').delete((req, res, next) => {
-  Quizz.findOneAndRemove(req.params.id, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.status(200).json({
-        msg: data
-      })
-    }
-  })
-});
+quizzRoutes.route('/delete/:id').patch((req, res, next) => {
+  console.log(req.params)
+  Quizz.remove({
+    _id:req.params.id
+ }, function(err,data){
+    if(err) return res.send(err);
+    
+    res.json({message:'Successfully deleted'});
+  });
+})
 module.exports = quizzRoutes;
